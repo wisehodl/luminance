3aea70110b1f5150a4b38ebb29ad8e1f063a75be
Jay <wisehodl@pm.me>
2025-06-14 12:07:54 -0400

    Removed boilerplate. Set css breakpoints.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import * as colorlib from "colorlib";
-import "./App.css";
-import style from "./App.module.css";
-import clsx from "clsx";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const color = colorlib.Color.from_hex("123123");
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={clsx(style.card)}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className={clsx(style.test)}>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>The color is: {color.hex.to_code()}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <div className={styles.appContainer}></div>;
 }
 
 export default App;
